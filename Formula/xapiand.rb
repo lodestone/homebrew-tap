29ec7945152d3bb94d121f591d9ecfe7e720332c
German M. Bravo <german.mb@gmail.com>
2018-04-03 16:33:25 -0500

    Make llvm@5 a build dependency

--- Formula/xapiand.rb
+++ Formula/xapiand.rb
@@ -9,15 +9,23 @@ class Xapiand < Formula
     root_url "https://kronuz.io/homebrew-tap"
     sha256 "cc9a169916464bc0c1fb255eb89d7cd8bfc4a088016ff301f9eb06ef70ae340c" => :high_sierra
     sha256 "f88fddf92416e0bc3a9b0de2f11d9fd4c0e509a4b271a923730ab969350eddeb" => :sierra
+    sha256 "5d697d5f1fa73cff6041b0d759430904c2063101dafaaaf62240b917d37ae97a" => :el_capitan
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "llvm@5" => :build
   depends_on "xapian"
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DCCACHE_FOUND=CCACHE_FOUND-NOTFOUND", *std_cmake_args
+      args = std_cmake_args + %W[
+        -DCMAKE_C_COMPILER=#{Formula["llvm@5"].opt_bin}/clang
+        -DCMAKE_CXX_COMPILER=#{Formula["llvm@5"].opt_bin}/clang++
+      ]
+      system "cmake", "..", "-DCCACHE_FOUND=CCACHE_FOUND-NOTFOUND", *args
+      system "make"
+      system "install_name_tool", "-change", "#{Formula["llvm@5"].opt_lib}/libc++.1.dylib", "/usr/lib/libc++.1.dylib", "bin/xapiand"
       system "make", "install"
     end
   end
