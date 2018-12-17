a926592a482685f922c10286dc20566b533e6f6e
Matt Petty <matt@kizmeta.com>
2018-12-17 15:41:07 -0600

    Update xapiand.rb

--- Formula/xapiand.rb
+++ Formula/xapiand.rb
@@ -1,16 +1,16 @@
 class Xapiand < Formula
   desc "Xapiand: A RESTful Search Engine"
   homepage "https://kronuz.io/Xapiand"
-  url "https://github.com/Kronuz/Xapiand/archive/v0.5.2.tar.gz"
-  sha256 "2b44f681da563537649620db1dd183f9ad1292a649302bd0a93c8108bf54524a"
+  url "https://github.com/Kronuz/Xapiand/archive/v0.8.2.tar.gz"
+  sha256 "1dddb91c6eede41b290871d72776684b87f2e6fc50a8261bacaf6bf81b94a7f7"
   head "https://github.com/Kronuz/Xapiand.git"
 
-  bottle do
-    root_url "https://kronuz.io/homebrew-tap"
-    sha256 "cc9a169916464bc0c1fb255eb89d7cd8bfc4a088016ff301f9eb06ef70ae340c" => :high_sierra
-    sha256 "f88fddf92416e0bc3a9b0de2f11d9fd4c0e509a4b271a923730ab969350eddeb" => :sierra
-    sha256 "5d697d5f1fa73cff6041b0d759430904c2063101dafaaaf62240b917d37ae97a" => :el_capitan
-  end
+  # bottle do
+  #   root_url "https://kronuz.io/homebrew-tap"
+  #   sha256 "cc9a169916464bc0c1fb255eb89d7cd8bfc4a088016ff301f9eb06ef70ae340c" => :high_sierra
+  #   sha256 "f88fddf92416e0bc3a9b0de2f11d9fd4c0e509a4b271a923730ab969350eddeb" => :sierra
+  #   sha256 "5d697d5f1fa73cff6041b0d759430904c2063101dafaaaf62240b917d37ae97a" => :el_capitan
+  # end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
